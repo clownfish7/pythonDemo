d475e17b0a7c0dd3a24a68b172b9549105caf8f7
clownfish7 <279505647@qq.com>
2020-12-27 20:13:44 +0800

    update register.html & register.js

--- meiduo_project/meiduo_mall/meiduo_mall/apps/users/static/js/register.js
+++ meiduo_project/meiduo_mall/meiduo_mall/apps/users/static/js/register.js
@@ -2,6 +2,7 @@
 let app = new Vue({
     // 通过 ID 选择器找到绑定 HTML 的内容
     el: '#app',
+    delimiters: ['[[', ']]'],
     // 数据绑定对象
     data: {
         // v-model
@@ -26,28 +27,53 @@ let app = new Vue({
         // 定义和实现事件方法
 
         // 校验用户名
-        check_username: () => {
-
+        check_username: function () {
+            let re = /^[a-zA-Z0-9_-]{5,20}$/;
+            if (re.test(this.username)) {
+                this.error_username = false;
+            } else {
+                this.error_username_message = '请输入5-20个字符的用户名';
+                this.error_username = true;
+            }
         },
         // 校验密码
-        check_password: () => {
-
+        check_password: function () {
+            let re = /^[0-9A-Za-z]{8,20}$/;
+            this.error_password = !re.test(this.password);
         },
         // 校验确认密码
-        check_password2: () => {
-
+        check_password2: function () {
+            this.error_password2 = this.password !== this.password2;
         },
         // 校验手机号
-        check_mobile: () => {
-
+        check_mobile: function () {
+            let re = /^1[3-9]\d{9}$/;
+            if (re.test(this.mobile)) {
+                this.error_mobile = false;
+            } else {
+                this.error_mobile_message = '您输入的手机号格式不正确';
+                this.error_mobile = true;
+            }
         },
         // 校验是否勾选协议
-        check_allow: () => {
-
+        check_allow: function () {
+            this.error_allow = !this.allow;
         },
         // 监听表单提交事件
-        on_submit: () => {
-
-        },
+        on_submit: function () {
+            this.check_username();
+            this.check_password();
+            this.check_password2();
+            this.check_mobile();
+            this.check_allow();
+            if (this.error_username === true ||
+                this.error_password === true ||
+                this.error_password2 === true ||
+                this.error_mobile === true ||
+                this.error_allow === true) {
+                // 禁用表单的提交
+                window.event.returnValue = false;
+            }
+        }
     }
 })
