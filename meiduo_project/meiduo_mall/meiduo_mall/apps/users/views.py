e8c9b7762fa05ce9e7fb8e5bfd940b4391e4c911
clownfish7 <279505647@qq.com>
2020-12-29 17:09:33 +0800

    ~

--- meiduo_project/meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_project/meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
-
+from django import http
+import re
 
 # Create your views here.
+from pymysql import DatabaseError
+
+from meiduo_project.meiduo_mall.meiduo_mall.apps.users.models import User
 
 
 class RegisterView(View):
@@ -12,8 +16,40 @@ class RegisterView(View):
         """提供用户注册页面"""
         return render(request, 'register.html')
 
-    def post(self):
-        pass
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
+
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 判断两次密码是否一致
+        if password != password2:
+            return http.HttpResponseForbidden('两次输入的密码不一致')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+
+        # 保存注册数据
+        try:
+            User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+
+        # 响应注册结果
+        return http.HttpResponse('注册成功')
 
     def delete(self):
         pass
